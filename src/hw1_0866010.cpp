e03dc9577b9fc6b625b177cb9e1e328be3e9697c
StanHsu0522 <kh3011657@gmail.com>
2020-03-22 15:33:43 +0800

    Test parse_opt function.... Done.

--- src/hw1_0866010.cpp
+++ src/hw1_0866010.cpp
@@ -2,18 +2,18 @@
 #include <getopt.h>     // for getopt_long
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 
-void parse_opt(int argc, char *argv[], map<string, bool> &show)
+void parse_opt(int argc, char *argv[], map<string, bool> &show, vector<string> &filter_strs)
 {
-    const char *optstring = "tu";
+    const char *optstring = "tu";       // for short options
     int c ;
     struct option opts[] = {
         {"tcp", 0,  NULL,   't'},
-        {"udp", 0,  NULL,   'u'},
-        {0,     0,  0,      0}      // unrecognized option
+        {"udp", 0,  NULL,   'u'}
     };
 
     for(unsigned int i=0 ; i<(sizeof(opts)/sizeof(option)) ; ++i){
@@ -23,6 +23,10 @@ void parse_opt(int argc, char *argv[], map<string, bool> &show)
 
 
     while((c = getopt_long(argc, argv, optstring, opts, NULL)) != -1){
+        
+        // cout << optopt << endl;      // By default, getopt() prints an error message on standard error,
+                                        // and places the erroneous option character in optopt, and returns '?'.
+
         switch (c)
         {
         case 't':
@@ -36,26 +40,45 @@ void parse_opt(int argc, char *argv[], map<string, bool> &show)
             exit(EXIT_FAILURE);
         }
     }
+
+    // have more args?
+    // optind is the index of the next element to be processed in argv[].
+    while(argc > optind){
+        filter_strs.push_back(argv[optind++]);
+    }
 }
 
 
 int main(int argc, char *argv[])
 {   
     map<string, bool> show;     // use to determine display either tcp or udp connections
+    vector<string> filter_strs;     // store string for filter function
+
 
     // parse options passed by user
-    parse_opt(argc, &argv[0], show);
+    parse_opt(argc, &argv[0], show, filter_strs);
 
-    // // map show traverse
-    // for(auto it = show.begin() ; it!=show.end() ; ++it){
-    //     if(it->second){
-    //         cout << "show " << it->first << endl;
-    //     }
-    //     else
-    //     {
-    //         cout << "don't show " << it->first << endl;
-    //     }
-    // }
+    // map show traverse
+    for(auto it = show.begin() ; it!=show.end() ; ++it){
+        if(it->second){
+            cout << "show " << it->first << endl;
+        }
+        else
+        {
+            cout << "don't show " << it->first << endl;
+        }
+    }
+
+    // vector filter_strs traverse
+    if(filter_strs.size() > 0){
+        cout << "filter_str: ";
+        for(auto it=filter_strs.begin() ; it!=filter_strs.end() ; ++it){
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+
+
 
     exit(EXIT_SUCCESS);
 }
